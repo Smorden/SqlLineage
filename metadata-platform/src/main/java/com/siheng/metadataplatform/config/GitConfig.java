3a7e6043b63e056389ee3d8a288f6d5ce3a95737
shiyuchao <shiyuchao@bizvane.cn>
2023-09-05 17:35:52 +0800

    git config提交

---
+++ metadata-platform/src/main/java/com/siheng/metadataplatform/config/GitConfig.java
@@ -0,0 +1,22 @@
+package com.siheng.metadataplatform.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Shinya
+ * @date 2023/9/5 5:08 下午
+ * @Desc
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "siheng.gitlab")
+public class GitConfig {
+
+    private String host;
+    private String accessToken;
+    private String namespace;
+    private String projectName;
+    private String branch;
+}
